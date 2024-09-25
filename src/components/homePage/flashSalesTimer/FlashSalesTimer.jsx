c1ea01796bf42c4b8ea4a569f4e5b46824160396
AgusT613 <torresagustin613@gmail.com>
2024-09-25 19:40:45 -0300

    feat(main): add timer and prev-next btn inside section layout

--- src/components/homePage/flashSalesTimer/FlashSalesTimer.jsx
+++ src/components/homePage/flashSalesTimer/FlashSalesTimer.jsx
@@ -1,7 +1,31 @@
 import styles from "@/components/homePage/shopSectionLayout/shopSectionLayout.module.css"
 
-export default function FlashSalesTimer() {
+const defaultTime = {day: "03", hour: "23", minute: "19", second: "56"}
+
+export default function FlashSalesTimer({time = defaultTime}) {
+    const { day, hour, minute, second } = time
+
     return (
-        <div className={styles.flashSalesTimerContainer}>TIMER</div>
+        <div className={styles.flashSalesTimerContainer}>
+            <div className={styles.timeContainer}>
+                <span className={styles.label}>Days</span>
+                <span className={styles.time}>{day}</span>
+            </div>
+
+            <div className={styles.timeContainer}>
+                <span className={styles.label}>Hours</span>
+                <span className={styles.time}>{hour}</span>
+            </div>
+
+            <div className={styles.timeContainer}>
+                <span className={styles.label}>Minutes</span>
+                <span className={styles.time}>{minute}</span>
+            </div>
+
+            <div className={styles.timeContainer}>
+                <span className={styles.label}>Seconds</span>
+                <span className={styles.time}>{second}</span>
+            </div>
+        </div>
     )
 };
--- src/components/homePage/slideBtn/SlideBtn.jsx
+++ src/components/homePage/slideBtn/SlideBtn.jsx
@@ -1,5 +1,25 @@
+import ArrowLeft from "@/assets/arrow-left.svg"
+import ArrowRight from "@/assets/arrow-right.svg"
+import styles from "@/components/homePage/slideBtn/slideBtn.module.css"
+
 export default function SlideBtn() {
+
+    const previousItem = () => {
+        return
+    }
+
+    const nextItem = () => {
+        return
+    }
+
     return (
-        <div>SLIDE</div>
+        <div className={styles.slideBtnContainer}>
+            <span className={styles.arrow} onClick={previousItem}>
+                <ArrowLeft />
+            </span>
+            <span className={styles.arrow} onClick={nextItem}>
+                <ArrowRight />
+            </span>
+        </div>
     )
 };
