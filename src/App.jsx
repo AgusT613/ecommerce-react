303e309243673aeadcc1370051ac01ba3957ac80
AgusT613 <torresagustin613@gmail.com>
2024-09-27 09:31:52 -0300

    feat(homePage): add best selling products section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import SlideBtn from './components/homePage/slideBtn/SlideBtn'
 import ProductsLayout from './components/homePage/productsLayout/ProductsLayout'
 import ViewAllProductsLink from './components/homePage/productsLayout/viewAllProductsLink/ViewAllProductsLink'
 import ProductsCategory from './components/homePage/productsCategory/ProductsCategory'
+import productItem from "/product-item.png"
 
 function App() {
 
@@ -29,11 +30,21 @@ function App() {
           </>
         }>
           <ProductsLayout>
-            <ViewAllProductsLink>View All Products</ViewAllProductsLink>
+            <div style={{padding: "2rem"}}>
+              <ViewAllProductsLink>View All Products</ViewAllProductsLink>
+            </div>
           </ProductsLayout>
         </ShopSectionLayout>
+
         <ShopSectionLayout title={"Categories"} subtitle={"Browse by Category"}>
           <ProductsCategory />
+        </ShopSectionLayout>
+
+        <ShopSectionLayout title={"This Month"} subtitle={"Best Selling Products"} sectionHeaderSlot={<ViewAllProductsLink />}>
+          <ProductsLayout limit={3}/>
+          <div className={styles.productItemImgContainer}>
+            <img src={productItem} alt="black speaker that enhaces your music experience" className={styles.productItemImg}/>
+          </div>
         </ShopSectionLayout>
       </main>
     </div>
--- src/components/homePage/productsLayout/viewAllProductsLink/ViewAllProductsLink.jsx
+++ src/components/homePage/productsLayout/viewAllProductsLink/ViewAllProductsLink.jsx
@@ -1,6 +1,6 @@
 import styles from "@/components/homePage/productsLayout/viewAllProductsLink/viewAllProductsLink.module.css"
 
-export default function ViewAllProductsLink({ children }) {
+export default function ViewAllProductsLink({ children = "View All" }) {
     return (
         <div className={styles.viewAllItemContainer}>
             <a href="#" className={styles.viewAllBtn}>{children}</a>
