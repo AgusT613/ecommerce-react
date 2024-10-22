76cdb98b01725b8d5883cd1d87f6dfe165e705c7
AgusT613 <torresagustin613@gmail.com>
2024-10-22 16:31:17 -0300

    feat(homePage): add timer on flash sale component

--- src/components/homePage/flashSalesTimer/FlashSalesTimer.jsx
+++ src/components/homePage/flashSalesTimer/FlashSalesTimer.jsx
@@ -1,9 +1,39 @@
 import styles from "@/components/homePage/shopSectionLayout/shopSectionLayout.module.css"
+import { useEffect, useState } from "react"
 
-const defaultTime = {day: "03", hour: "23", minute: "19", second: "56"}
+export default function FlashSalesTimer({ days = 3 }) {
+    const [day, setDay] = useState(days)
+    const [hour, setHour] = useState(23)
+    const [minute, setMinute] = useState(59)
+    const [second, setSeconds] = useState(60)
 
-export default function FlashSalesTimer({time = defaultTime}) {
-    const { day, hour, minute, second } = time
+    useEffect(()=> {
+        const intervalId = setInterval(() => {
+            if (second === 0) {
+                setMinute(minute - 1)
+                setSeconds(60)
+            } else setSeconds(second - 1)
+            
+            if (second === 0 && minute === 0) {
+                setHour(hour - 1)
+                setMinute(59)
+                setSeconds(60)
+            }
+            
+            if (second === 0 && minute === 0 && hour === 0) {
+                setDay(day - 1)
+                setHour(23)
+                setMinute(59)
+                setSeconds(60)
+        }
+        }, 1000);
+
+        if (day === 0 && second === 0) clearInterval(intervalId)
+
+        return ()=> {
+            clearInterval(intervalId)
+        }
+    }, [second])
 
     return (
         <div className={styles.flashSalesTimerContainer}>
