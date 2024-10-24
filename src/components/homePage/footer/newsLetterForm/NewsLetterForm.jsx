66a2035f0af9341e05eebf1a0970213108692442
AgusT613 <torresagustin613@gmail.com>
2024-10-24 11:50:39 -0300

    feat(homePage): add newsletter email subscription with localStorage

--- src/components/homePage/footer/newsLetterForm/NewsLetterForm.jsx
+++ src/components/homePage/footer/newsLetterForm/NewsLetterForm.jsx
@@ -5,11 +5,23 @@ export default function NewsLetterForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const dataLabel = "newsLetterUsers"
+
+        const { email } = Object.fromEntries(new FormData(e.target))
+        const usersAlreadySubscribed = JSON.parse(localStorage.getItem(dataLabel)) || []
+
+        if (usersAlreadySubscribed.includes(email)){
+            alert("User has already subscribed")
+        } else {
+            usersAlreadySubscribed.push(email)
+            localStorage.setItem(dataLabel, JSON.stringify(usersAlreadySubscribed))
+            alert("Thanks for subscribing")
+        }
     }
 
     return (
         <form className={styles.form} onSubmit={handleSubmit}>
-            <input type="email" placeholder='Enter your email' />
+            <input type="email" placeholder='Enter your email' name="email"/>
             <button>
                 <SendIcon />
             </button>
