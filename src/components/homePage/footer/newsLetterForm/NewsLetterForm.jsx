9ea5b285b35f31a4356908ef37cf0e6a298b3aaa
AgusT613 <torresagustin613@gmail.com>
2024-10-25 16:29:27 -0300

    feat(homePage): add toast notification to newsLetter component

--- src/components/homePage/footer/newsLetterForm/NewsLetterForm.jsx
+++ src/components/homePage/footer/newsLetterForm/NewsLetterForm.jsx
@@ -1,21 +1,33 @@
 import SendIcon from "@/assets/send.svg"
 import styles from "@/components/homePage/footer/newsLetterForm/newsLetterForm.module.css"
+import { useToast } from "@/hooks/useToast"
 
 export default function NewsLetterForm() {
+    const toast = useToast()
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const dataLabel = "newsLetterUsers"
 
-        const { email } = Object.fromEntries(new FormData(e.target))
+        const form = e.target
+        const { email } = Object.fromEntries(new FormData(form))
         const usersAlreadySubscribed = JSON.parse(localStorage.getItem(dataLabel)) || []
 
         if (usersAlreadySubscribed.includes(email)){
-            alert("User has already subscribed")
+            toast({
+                message: "User has already subscribed",
+                type: "error",
+                time: 4000
+            })
         } else {
             usersAlreadySubscribed.push(email)
             localStorage.setItem(dataLabel, JSON.stringify(usersAlreadySubscribed))
-            alert("Thanks for subscribing")
+            form.reset()
+            toast({
+                message: "Thanks for subscribing",
+                type: "success",
+                time: 4000
+            })
         }
     }
 
